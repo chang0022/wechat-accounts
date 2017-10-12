615b686892858fe123d86dd7dce6b4da67b6d168
chang0022 <chang.0022@163.com>
2017-10-12 14:22:11 +0800

    上传临时图片

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -1,26 +1,39 @@
 'use strict'
 
 const util = require('util');
+const fs = require('fs');
 const request = util.promisify(require('request'));
 const wechat_util = require('./wechat_util');
 
 const prefix = 'https://api.weixin.qq.com/cgi-bin/';
 const api = {
-    accessToken: prefix + 'token?grant_type=client_credential'
+    accessToken: prefix + 'token?grant_type=client_credential',
+    upload: prefix + 'media/upload?'
 };
 
-function Wechat (opts) {
-    var self = this;
+function Wechat(opts) {
     this.appID = opts.appID;
     this.appSecret = opts.appSecret;
     this.getAccessToken = opts.getAccessToken;
     this.saveAccessToken = opts.saveAccessToken;
 
+    this.fetchAccessToken();
+}
+
+Wechat.prototype.fetchAccessToken = function (data) {
+    var self = this;
+
+    if (this.access_token && this.expires_in) {
+        if (this.isValidAccessToken(this)) {
+            return Promise.resolve(this);
+        }
+    }
+
     this.getAccessToken()
         .then(data => {
             try {
                 data = JSON.parse();
-            } catch(e) {
+            } catch (e) {
                 return self.updateAccessToken();
             }
 
@@ -35,11 +48,12 @@ function Wechat (opts) {
             self.expires_in = data.expires_in;
 
             self.saveAccessToken(data);
+
+            return Promise.resolve(data);
         })
 }
-
-Wechat.prototype.isValidAccessToken = function(data) {
-    if(!data || !data.access_token || !data.expires_in)  {
+Wechat.prototype.isValidAccessToken = function (data) {
+    if (!data || !data.access_token || !data.expires_in) {
         return false;
     }
 
@@ -47,20 +61,20 @@ Wechat.prototype.isValidAccessToken = function(data) {
     const expires_in = data.expires_in;
     const now = new Date().getTime();
 
-    if( now < expires_in) {
+    if (now < expires_in) {
         return true;
     } else {
         return false;
     }
 }
 
-Wechat.prototype.updateAccessToken = function() {
-    let appID = this.appID;
-    let appSecret = this.appSecret;
+Wechat.prototype.updateAccessToken = function () {
+    const appID = this.appID;
+    const appSecret = this.appSecret;
     const url = api.accessToken + `&appid=${appID}&secret=${appSecret}`;
 
     return new Promise((resolve, reject) => {
-        request({url: url, json: true}).then(res => {
+        request({ url: url, json: true }).then(res => {
             const data = res.body;
             const now = new Date().getTime();
             const expires_in = now + (data.expires_in - 20) * 1000;
@@ -71,7 +85,36 @@ Wechat.prototype.updateAccessToken = function() {
     });
 }
 
-Wechat.prototype.reply = function(ctx) {
+Wechat.prototype.uploadMaterial = function (type, filepath) {
+    var self = this;
+    const form = {
+        media: fs.createReadStream(filepath)
+    }
+
+    return new Promise((resolve, reject) => {
+        self
+            .fetchAccessToken()
+            .then(data => {
+                const url = api.upload + `access_token=${data.access_token}&type=${type}`;
+
+                request({ method: 'POST', url: url, formData: form, json: true })
+                    .then(res => {
+                        const _data = res.body;
+                        if (_data) {
+                            resolve(_data);
+                        } else {
+                            throw new Error('Upload material fails')
+                        }
+                    })
+                    .catch(err => {
+                        reject(err);
+                    });
+            })
+
+    });
+}
+
+Wechat.prototype.reply = function (ctx) {
     var content = ctx.body;
     var message = ctx.weixin;
 
--- weixin.js
+++ weixin.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const config = require('./config')
+const Wechat = require('./wechat/wechat');
+const wechatApi = new Wechat(config.wechat);
+
 exports.reply = async (ctx, next) => {
     const message = ctx.weixin;
 
@@ -8,13 +12,89 @@ exports.reply = async (ctx, next) => {
             if (message.EventKey) {
                 console.log('扫描二维码' + message.EventKey + ' ' + message.ticket);
             }
-            ctx.body = '哈哈，欢迎订阅\r\n' + ' 消息来自老常';
+            ctx.body = '哈哈，欢迎订阅\r\n' + '-----消息来自老常';
         } else if (message.Event === 'unsubscribe') {
             console.log('无情取关！');
             ctx.body = '';
+        } else if (message.Event === 'LOCATION') {
+            ctx.body = '你的地理位置是：' + message.Latitude + '/' + message.Longitude + '-' + message.Precision;
+        } else if (message.Event === 'CLICK') {
+            ctx.body = '你点击了菜单：' + message.EventKey;
+        } else if (message.Event === 'SCAN') {
+            console.log('关注后扫二维码' + message.EventKey + ' ' + message.Ticket);
+            ctx.body = '看到你扫了一下哦';
+        } else if (message.Event === 'VIEW') {
+            ctx.body = '你点击了菜单中的链接：' + message.EventKey;
         }
-    } else {
+    } else if (message.MsgType === 'text') {
+        const content = message.Content;
+        let reply = null;
 
+        switch (content) {
+            case '1':
+                reply = '一心敬';
+                break;
+            case '2':
+                reply = '哥俩好';
+                break;
+            case '3':
+                reply = '三桃园';
+                break;
+            case '4':
+                reply = '四季财';
+                break;
+            case '5':
+                reply = '五魁首';
+                break;
+            case '6':
+                reply = '六六顺';
+                break;
+            case '7':
+                reply = '七仙女';
+                break;
+            case '8':
+                reply = '八匹马';
+                break;
+            case '9':
+                reply = '九盅酒';
+                break;
+            case '10':
+                reply = '十全美';
+                break;
+            case '慕课':
+                reply = [
+                    {
+                        title: '7天搞定Node.js微信公众号',
+                        description: 'Koa框架、ES2015新特性、MongoDB，开发微信公众号',
+                        picurl: 'http://img1.sycdn.imooc.com/szimg/576376520001da3d05400300-360-202.jpg',
+                        url: 'http://coding.imooc.com/class/38.html'
+                    },
+                    {
+                        title: 'Node.js入门到企业Web开发中的应用',
+                        description: 'Node.js是Web应用开发的一个福音，特别适合中小型系统的快速开发！',
+                        picurl: 'http://img1.sycdn.imooc.com/szimg/59c860220001247d05400300-360-202.jpg',
+                        url: 'http://coding.imooc.com/class/146.html'
+                    },
+                    {
+                        title: '全网稀缺Vue 2.0高级实战 独立开发专属音乐WebAPP',
+                        description: '独一无二的Vue 2.0高级实战课程，达到百度T4水平',
+                        picurl: 'http://img1.sycdn.imooc.com/szimg/592e2eab0001302505660314-360-202.jpg',
+                        url: 'http://coding.imooc.com/class/107.html'
+                    }
+                ];
+                break;
+            case '图片':
+                const data = await wechatApi.uploadMaterial('image', __dirname + '/image/avatar.jpg');
+                reply = {
+                    type: 'image',
+                    mediaId: data.media_id
+                }
+                break;
+            default:
+                reply = '你的说：' + message.Content + '不明白';
+        }
+
+        ctx.body = reply;
     }
 
     await next();
