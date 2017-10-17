9a0649a9351e771401354525999bbae241b7ead5
chang0022 <chang.0022@163.com>
2017-10-17 22:40:56 +0800

    需要真实环境模拟

--- app.js
+++ app.js
@@ -15,37 +15,69 @@ const Wechat = require('./wechat/wechat')
 const app = new Koa();
 
 const tpl = heredoc(() => {/*
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>猜电影</title>
-    </head>
-    <body>
-        <h1>点击标题，开始录音翻译</h1>
-        <p id="title"></p>
-        <div id="poster"></div>
-    </body>
-    <script src="https://cdn.bootcss.com/zepto/1.2.0/zepto.min.js"></script>
-    <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
-    <script>
-        wx.config({
-            debug: true,
-            appId: 'wx001e2f72ebd7a312',
-            timestamp: <%= timestamp %>,
-            nonceStr: '<%= nonceStr %>',
-            signature: '<%= signature %>',
-            jsApiList: [
-                'startRecord',
-                'stopRecord',
-                'onVoiceRecordEnd',
-                'translateVoice'
-            ]
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>猜电影</title>
+</head>
+<body>
+    <h1 id="h1">点击标题，开始录音翻译</h1>
+    <p id="title"></p>
+    <div id="directors"></div>
+    <div id="year"></div>
+    <div id="poster"></div>
+</body>
+<script src="https://cdn.bootcss.com/zepto/1.2.0/zepto.min.js"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+<script>
+wx.config({
+    debug: false,
+    appId: 'wx001e2f72ebd7a312',
+    timestamp: '<%= timestamp %>',
+    nonceStr: '<%= nonceStr %>',
+    signature: '<%= signature %>',
+    jsApiList: [
+        'startRecord',
+        'stopRecord',
+        'onVoiceRecordEnd',
+        'translateVoice'
+    ]
+});
+wx.ready(function() {
+    var isRecording = false;
+    $('#h1').click(function() {
+        if(!isRecording) {
+            isRecording = true;
+            wx.startRecord({
+                cancel: function() {
+                    console.log('请开启麦克风');
+                }
+            });
+
+            return '';
+        }
+        isRecording = false;
+        wx.stopRecord({
+            success: function (res) {
+                var localId = res.localId;
+
+                wx.translateVoice({
+                    localId: localId,
+                    isShowProgressTips: 1,
+                    success: function (res) {
+                        console.log(res.translateResult);
+                        alert(res.translateResult);
+                    }
+                });
+            }
         });
-    </script>
-    </html>
+    });
+});
+</script>
+</html>
 */});
 
 const createNonceStr = () => {
